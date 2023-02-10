8862c57464b8e6ff96221c8c9453d7912e863131
martingale <124511455+martingale-2910@users.noreply.github.com>
2023-02-10 14:50:52 +0100

    Simplify the code

--- src/main.cxx
+++ src/main.cxx
@@ -35,8 +35,6 @@ struct BS
     double r;
     double vol;
 
-    // TODO: How to work-around the necessity of having a dft constructor?
-    BS() = default;
     BS(double r, double vol): r(r), vol(vol) {};
 
     double discount_factor(double t) const
@@ -47,15 +45,16 @@ struct BS
 
 // MODEL SIMULATIONS
 
-vecd compute_bs_step(BS const & bs, vecd & s, double dt, vecd const & z)
+vecd compute_model_step(BS const & bs, vecd & s, double dt, vecd const & z)
 {
     s = std::move(s * (1 + bs.r * dt + bs.vol * std::sqrt(dt) * z));
     return s;
 };
 
-vecd simulate_bs_values(BS const & bs, double s0, double dt, uint npaths, uint nsteps)
+vecd simulate_model_values(BS const & bs, vecd const & s0, double dt, uint nsteps)
 {
-    vecd st = vecd(s0, npaths);
+    vecd st = s0;
+    std::size_t npaths = s0.size();
     if (DEBUG)
     {
         std::cout << "[0]" <<  " st := " << st.sum() / st.size() << std::endl;
@@ -63,7 +62,7 @@ vecd simulate_bs_values(BS const & bs, double s0, double dt, uint npaths, uint n
     for(uint i = 0; i < nsteps; ++i)
     {
         vecd z = generate_stdnorm(npaths);
-        st = std::move(st * (1 + bs.r * dt + bs.vol * std::sqrt(dt) * z));
+        st = std::move(compute_model_step(bs, st, dt, z));
         if (DEBUG)
         {
             std::cout << "[" << i + 1 << "]" <<  " st := " << st.sum() / st.size() << std::endl;
@@ -80,28 +79,7 @@ enum class Right
 };
 
 template<Right right>
-struct Payoff;
-
-template<>
-struct Payoff<Right::CALL>
-{
-    static double compute(double s, double k)
-    {
-        return std::max(s - k, 0.0);
-    };
-};
-
-template<>
-struct Payoff<Right::PUT>
-{
-    static double compute(double s, double k)
-    {
-        return std::max(k - s, 0.0);
-    }; 
-};
-
-template<Right right>
-constexpr auto vanilla_payoff = &(Payoff<right>::compute);
+constexpr auto vanilla_payoff = [](double s, double k)->double { return right == Right::CALL ? std::max(s - k, 0.0) : std::max(k - s, 0.0); };
 
 enum class Style
 {
@@ -114,47 +92,30 @@ struct Option
     double strike;
     double ttm;
 
-    // TODO: How to work-around the necessity of having a dft constructor?
-    Option() = default;
     Option(double strike, double ttm): strike(strike), ttm(ttm) {};
     double payoff(double s) const { return vanilla_payoff<right>(s, this->strike); };
     vecd payoff(vecd const & s) const { return (s - this->strike).apply([](double xi)->double{ return vanilla_payoff<right>(xi, 0.0); }); };
 };
 
-using EuropeanCall = Option<Right::CALL, Style::EUROPEAN>;
-using EuropeanPut = Option<Right::PUT, Style::EUROPEAN>;
+template<Right right>
+using EuropeanOption = Option<right, Style::EUROPEAN>;
+using EuropeanCall = EuropeanOption<Right::CALL>;
+using EuropeanPut = EuropeanOption<Right::PUT>;
 
-using AmericanCall = Option<Right::CALL, Style::AMERICAN>;
-using AmericanPut = Option<Right::PUT, Style::AMERICAN>;
+template<Right right>
+using AmericanOption = Option<right, Style::AMERICAN>;
+using AmericanCall = AmericanOption<Right::CALL>;
+using AmericanPut = AmericanOption<Right::PUT>;
 
 // MC VALUE COMPUTATIONS
 
-// TODO: How to work-around the necessity of having a constructor?
-template<typename Model, typename Option>
-struct MCValue
+template<Right right>
+double compute_mc_price(BS const & model, EuropeanOption<right> const & option, double s0, uint npaths, uint nsteps)
 {
-    MCValue(Model const & model, Option const & option);
-};
-
-template<typename EuropeanOption>
-struct MCValue<BS, EuropeanOption>
-{
-    BS model;
-    EuropeanOption option;
-
-    MCValue(BS const & model, EuropeanOption const & option)
-    {
-        this->model = model;
-        this->option = option;
-    };
-
-    double compute(double s0, uint npaths, uint nsteps) const
-    {
-        double dt = this->option.ttm / nsteps;
-        vecd st = simulate_bs_values(this->model, s0, dt, npaths, nsteps);
-        vecd v0 = this->model.discount_factor(this->option.ttm)*(this->option).payoff(st);
-        return v0.sum() / v0.size();
-    };
+    double dt = option.ttm / nsteps;
+    vecd st = std::move(simulate_model_values(model, vecd(s0, npaths), dt, nsteps));
+    vecd v0 = std::move(model.discount_factor(option.ttm)*(option.payoff(st)));
+    return v0.sum() / v0.size();
 };
 
 int main(int argc, char** argv)
@@ -174,8 +135,8 @@ int main(int argc, char** argv)
     uint nsteps = 240;
 
     std::cout << "Using seed " << seed << std::endl;
-    double val1 = MCValue(model, callopt).compute(s0, npaths, nsteps);
-    double val2 = MCValue(model, putopt).compute(s0, npaths, nsteps);
+    double val1 = compute_mc_price(model, callopt, s0, npaths, nsteps);
+    double val2 = compute_mc_price(model, putopt, s0, npaths, nsteps);
     std::cout << "[CALL] mc_value := " << val1 << std::endl;
     std::cout << "[PUT] mc_value := " << val2 << std::endl;
 
